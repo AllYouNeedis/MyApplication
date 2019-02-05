5336f1c9942d063d8b73212ac4c2b7ae30ea0443
Yan Herasimau <SuperMoon15@yandex.by>
2019-02-05 20:44:15 +0300

    feature:test 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# MyApplication
+CircleCI - [![CircleCI](https://circleci.com/gh/AllYouNeedis/MyApplication.svg?style=svg)](https://circleci.com/gh/AllYouNeedis/MyApplication)
+
+AppCenter - [![Build status](https://build.appcenter.ms/v0.1/apps/5ef51687-e616-48a4-80c1-c177b8891f94/branches/master/badge)](https://appcenter.ms)
--- app/src/main/java/com/example/HerasimauYanApp/MainActivity.java
+++ app/src/main/java/com/example/HerasimauYanApp/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.HerasimauYanApp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,6 +13,7 @@ import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
 import com.microsoft.appcenter.distribute.Distribute;
+
 
 import io.fabric.sdk.android.Fabric;
 
@@ -29,21 +27,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         AppCenter.start(getApplication(), "APP_CENTER_KEY",
                 Analytics.class, Crashes.class);
         AppCenter.start(getApplication(), APP_CENTER_KEY, Distribute.class);
 
         final TextView version = findViewById(R.id.version);
         version.setText(BuildConfig.VERSION_NAME);
+
     }
 
     @Override
@@ -67,4 +57,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
